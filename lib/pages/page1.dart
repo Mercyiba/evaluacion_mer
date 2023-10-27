b61a39b52be736144ad68de3b4b0c4e43bf3c798
Mercyibañez <106113572+Mercyiba@users.noreply.github.com>
2023-10-26 18:32:48 -0600

    colores

--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Calcular Altura de caida',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.pink,
       ),
       home: AlturaCalculadora(),
     );
